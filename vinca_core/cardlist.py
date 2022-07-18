235ba36575e421e98d3b11cbf6d9b4dea703feb0
Oscar Laird <olaird25@gmail.com>
2022-07-18 13:25:33 -0600

    gitignore dist

--- vinca_core/cardlist.py
+++ vinca_core/cardlist.py
@@ -160,14 +160,14 @@ Read `filter --help` for a complete list of predicates'''
                              'time': ' ORDER BY (edit_seconds + review_seconds)',
                              'total time': ' ORDER BY (edit_seconds + review_seconds)',
                              'recent': ' ORDER BY max(last_review_date, last_edit_date)',
-                             'meritorious': ' ORDER BY merit',
+                             'merit': ' ORDER BY merit',
                               }
                 if criterion not in crit_dict:
                         return f'supply a criterion: {" | ".join(crit_dict.keys())}'
                 new_cardlist = self._copy()
                 new_cardlist._ORDER_BY = crit_dict[criterion]
                 # Sometimes it is natural to see the highest value first by default
-                reverse ^= criterion in ('recent', 'time', 'total time', 'meritorious') 
+                reverse ^= criterion in ('recent', 'time', 'total time', 'merit') 
                 direction = ' DESC' if reverse else ' ASC'
                 new_cardlist._ORDER_BY += direction
                 return new_cardlist
